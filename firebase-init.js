3a90f5664c21bcd13299dd85f323ee37adac5d62
Eduardo Lipoli <eduardolipoli95@gmail.com>
2025-04-01 11:09:50 -0400

    Removendo firebase-init.js do rastreamento

--- firebase-init.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBExhgeuG6-eHhVuarUPfubnpRn4OPdHMs",
-    authDomain: "controle-de-gastos-7e539.firebaseapp.com",
-    projectId: "controle-de-gastos-7e539",
-    storageBucket: "controle-de-gastos-7e539.firebasestorage.app",
-    messagingSenderId: "693947952417",
-    appId: "1:693947952417:web:8aac7ea3fb469947426260"
-};
-firebase.initializeApp(firebaseConfig);
